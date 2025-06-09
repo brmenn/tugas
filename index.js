0321beb11b6a84c5dfd7ebcd9a1a3b56e311019b
brmenn <137709370+brmenn@users.noreply.github.com>
2025-06-10 06:28:17 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,18 +8,10 @@ window.addEventListener("load", () => {
 
     setTimeout(() => {
       preloader.style.pointerEvents = "none";
-      preloader.style.zIndex = "-1";
+      preloader.style.zIndex = "-3";
       preloader.style.display = "none";
       content.style.display = "block";
     }, 100);
   }, 1000);
-});
-
-document.addEventListener('mousemove', function (e) {
-  const hero = document.querySelector('.hero-section');
-  const bg = document.querySelector('.hero-bg-3d');
-  const x = (e.clientX / window.innerWidth - 0.5) * 20;
-  const y = (e.clientY / window.innerHeight - 0.5) * 20;
-  bg.style.transform = `scale(1.1) rotateX(${y}deg) rotateY(${x}deg)`;
 });
 
